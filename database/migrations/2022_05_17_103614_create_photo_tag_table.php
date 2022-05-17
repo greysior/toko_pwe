9bb20c41a4d69f1383f5fd4506d3fe637aad8800
greysior <gramadhan877@gmail.com>
2022-05-17 17:58:49 +0700

    erd-dea

---
+++ database/migrations/2022_05_17_103614_create_photo_tag_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotoTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('photo_id')->constrained('photos');
+            $table->foreignId('tag_id')->constrained('tag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_tag');
+    }
+}
